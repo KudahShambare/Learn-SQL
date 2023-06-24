849440b4f470d392cab0e4e17c395718718115d4
KudahShambare <Kudahshambare263@gmail.com>
2023-06-24 21:00:40 +0200

    up to task 5 done

--- My Solutions/Task4.sql
+++ My Solutions/Task4.sql
@@ -14,3 +14,11 @@ Return the band name as `Band Name`.
 | Within Temptation | 
 | Death             | 
 | Van Canto         | 
+
+select DISTINCT  bands.band_name as "Band Name"
+from albums 
+inner join bands 
+on bands.id  = albums.band_id 
+ORDER  by band_name 
+
+/*correct answer  - i modified my query to sort the band names in an alphabetical order*/
--- My Solutions/Task5.sql
+++ My Solutions/Task5.sql
@@ -10,3 +10,13 @@ Return the band name as `Band Name`.
 | Band Name     | 
 |---------------| 
 | Dream Theater | 
+
+select   bands.band_name as "Band Name"
+from bands 
+except 
+select   bands.band_name as "Band Name"
+from albums 
+inner join bands 
+on bands.id  = albums.band_id 
+
+/*Correct Answer*/
