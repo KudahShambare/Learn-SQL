2ff7ffc951633e1d29958b0d974d7c64b9704e40
KudahShambare <Kudahshambare263@gmail.com>
2023-06-25 01:00:34 +0200

    task 6 done

--- My Solutions/Task6.sql
+++ My Solutions/Task6.sql
@@ -10,3 +10,15 @@ Return the album name as `Name`, the album release year as `Release Year`, and t
 | Name           | Release Year | Duration          | 
 |----------------|--------------|-------------------| 
 | Death Magnetic | 2008         | 74.76666593551636 | 
+
+
+select   albums.name as Name, albums.release_year as "Release Year",sum(songs.length) as Duration
+from songs 
+inner join albums 
+on songs.album_id  = albums.id
+group by albums.name, albums.release_year 
+order by sum(songs.`length`) desc
+limit 1;
+
+
+/*Correct Solution*/
